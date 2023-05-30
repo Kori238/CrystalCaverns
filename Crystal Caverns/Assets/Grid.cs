2b8c14882e24e8baeebb227ad6a351a537c9a4f1
Kori238 <jakeedwards238@gmail.com>
2023-05-30 18:40:07 +0100

    unity init
    Signed-off-by: Kori238 <jakeedwards238@gmail.com>

---
+++ Crystal Caverns/Assets/Grid.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        List<int> testList = new();
+        var yes = testList;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
